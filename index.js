21875cf12bfafc438416267b7fae15e41ade2e4c
JYB <7303639+BingoJYB@users.noreply.github.com>
2022-11-04 02:24:18 +0100

    Use middleware to route

--- index.js
+++ index.js
@@ -3,4 +3,14 @@ let express = require('express');
 
 let app = express();
 
-http.createServer(app).listen(8080);
+app.use("/add-product", (req, res, next) => {
+  console.log("Call '/add-product'");
+  res.send("<h1>Call '/add-product'</h1>")
+});
+
+app.use("/", (req, res, next) => {
+  console.log("Call '/'");
+  res.send("<h1>Call '/'</h1>")
+});
+
+app.listen(8080);
