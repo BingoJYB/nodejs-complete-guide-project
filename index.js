f4e0f2ec05e7f484abbdc082f3ba7bd512e51299
JYB <7303639+BingoJYB@users.noreply.github.com>
2022-11-05 02:55:42 +0100

    Serve css styling

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const adminRouter = require('./routers/admin');
 const shopRouter = require('./routers/shop');
+const rootDir = require('./util/path');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, 'public')));
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(8080);
