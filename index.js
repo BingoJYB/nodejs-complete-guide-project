580996e6e979ee118440c04b677992c96090cbf7
JYB <7303639+BingoJYB@users.noreply.github.com>
2022-11-07 00:10:18 +0100

    Store and fetch data from file

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
 const adminRouter = require('./routers/admin');
 const shopRouter = require('./routers/shop');
-const rootDir = require('./util/path');
 const errorsController = require('./controllers/errors');
 
 const app = express();
@@ -11,7 +9,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(rootDir, 'public')));
+app.use(express.static('./public'));
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
