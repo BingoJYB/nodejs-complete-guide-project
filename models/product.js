2d03f0f121d78a11b422232e9097e674dc376325
JYB <7303639+BingoJYB@users.noreply.github.com>
2022-12-11 23:26:43 +0100

    Add delete cart items functionality

--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart');
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
@@ -47,6 +49,18 @@ module.exports = class Product {
     });
   }
 
+  static deleteById(id) {
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(prod => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+        }
+      });
+    });
+  }
+
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
@@ -55,21 +69,6 @@ module.exports = class Product {
     getProductsFromFile(products => {
       const product = products.find(p => p.id === id);
       cb(product);
-    });
-  }
-
-  static deleteById(id, cb) {
-    getProductsFromFile(products => {
-      const updatedProducts = products.filter(product => {
-        return product.id !== id;
-      });
-      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-        if (err) {
-          console.log(err);
-        } else {
-          cb(updatedProducts);
-        }
-      });
     });
   }
 };
