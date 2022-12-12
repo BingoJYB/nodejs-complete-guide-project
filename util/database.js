29684e50f0e7ddb4d4d0a3df2191ea8ef0d1adc8
JYB <7303639+BingoJYB@users.noreply.github.com>
2022-12-13 00:05:54 +0100

    Add create and drop table sql

--- util/database.js
+++ util/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'node-complete',
-    password: 'nodecomplete'
+    password: 'root',
+    database: 'node_complete'
 });
 
 module.exports = pool.promise();
